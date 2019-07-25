e79334fc5d881ed128e16c9861cfcdc24ca0456b
melissaortiz <melissaortiz.dev@gmail.com>
2019-07-25 13:15:27 -0700

    connected Firebase to React

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import Firebase from './components/Firebase';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <firebaseContext.Provider value={new Firebase()}>
+    
+    <App />,
+    </firebaseContext.Provider >,
+     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
