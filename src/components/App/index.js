fc54792b313eb839b22e7d48d5a12bc0dd9469e8
melissaortiz <melissaortiz.dev@gmail.com>
2019-08-06 12:21:11 -0700

    fixed error message, was a missing underscore...

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { 
   BrowserRouter as Router,
-  Route,
-  Link
+  Route
+  //Link
 } from 'react-router-dom';
 
 import Navigation from '../Navigation'
--- src/index.js
+++ src/index.js
@@ -2,18 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import Firebase from './components/Firebase';
+
+import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 ReactDOM.render(
-    <firebaseContext.Provider value={new Firebase()}>
-    
-    <App />,
-    </firebaseContext.Provider >,
-     document.getElementById('root'));
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root'),
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
