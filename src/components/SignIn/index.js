875ca2043ec9d4f0c477512159cb418d92de6181
melissaortiz <melissaortiz.dev@gmail.com>
2019-08-16 12:26:38 -0700

    password forget/reset

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router-dom'
 import { compose } from 'recompose'
 
 import { SignUpLink } from '../SignUp'
+import { PasswordForgetLink } from '../PasswordForget'
 import { withFirebase } from '../Firebase'
 import * as ROUTES from '../../constants/routes'
 
@@ -11,6 +12,7 @@ const SignInPage = () => (
     <div>
         <h1>Sign in</h1>
         <SignInForm />
+        <PasswordForgetLink />
         <SignUpLink />
     </div>
 )
@@ -59,7 +61,7 @@ class SignInFormBase extends Component {
             type="text"
             placeholder="Email Address"
             />
-            <input
+            <input     
             name="password"
             value={password}
             onChange={this.onChange}
